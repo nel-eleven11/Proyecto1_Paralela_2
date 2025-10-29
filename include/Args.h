75d2969c9f523f533912a047520bfee83644fa3b
Joaquín Puente <joaquin.puenteg@outlook.com>
2025-10-29 15:17:31 -0600

    Inclusión de headers para la app

---
+++ include/Args.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+struct Config {
+    int   width  = 1280;
+    int   height = 720;
+    int   n      = 400;
+    float radius = 120.f;
+    float speed  = 1.f;
+    bool  parallel = false;
+    unsigned int seed = 0;
+    int   threads = 0;
+    bool  bench   = false;
+};
+
+bool parseArgs(int argc, char** argv, Config& out, std::string& error);
+void printHelp();
